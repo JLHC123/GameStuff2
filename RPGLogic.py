99b29d87469b7612c2f3890a60e0a19e7cd61424
JLHC123 <jlhc3751@gmail.com>
2026-02-03 16:57:22 -0800

    added a describe function which makes a string that can be printed which displays an entities's (like player or boss) description

--- RPGLogic.py
+++ RPGLogic.py
@@ -1,12 +1,23 @@
-def main():
-    player = Entity("Player", 100)
-    print(player.name)
-    
-
 class Entity:
     def __init__ (self, name, health):
         self.name = name
         self.health = health
 
+def describe(entity):
+    # creates string description of entity
+    return (f"{entity.name}\nHealth: {entity.health}")
+
+def main():
+    # create entities
+    player = Entity("Player", 100)    
+    boss = Entity("Boss", 500)
+    
+    # get descriptions
+    player_description = describe(player)
+    print(player_description)
+    boss_description = describe(boss)
+    print(boss_description)
+    
 if __name__ == "__main__":
     main()
+
