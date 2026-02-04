953311643fa507d7febf5c27bd0949f2a2318a4a
JLHC123 <jlhc3751@gmail.com>
2026-02-03 17:03:23 -0800

    added a attack function that lets entities damage other entities (boss attacking player)

--- RPGLogic.py
+++ RPGLogic.py
@@ -1,16 +1,21 @@
 class Entity:
-    def __init__ (self, name, health):
+    def __init__ (self, name, health, attack):
         self.name = name
         self.health = health
+        self.attack = attack
 
 def describe(entity):
     # creates string description of entity
-    return (f"{entity.name}\nHealth: {entity.health}")
+    return (f"{entity.name}\nHealth: {entity.health}\nAttack: {entity.attack}")
+
+def attack(defender, attacker):
+    defender.health -= attacker.attack
+    print(f"{attacker.name} attacks {defender.name} for {attacker.attack} damage!")
 
 def main():
     # create entities
-    player = Entity("Player", 100)    
-    boss = Entity("Boss", 500)
+    player = Entity("Player", 100, 10)    
+    boss = Entity("Boss", 500, 50)
     
     # get descriptions
     player_description = describe(player)
@@ -18,6 +23,8 @@ def main():
     boss_description = describe(boss)
     print(boss_description)
     
+    attack(player, boss)    
+
 if __name__ == "__main__":
     main()
 
